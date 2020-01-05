bdebe45d1670aabff3f7fea72fd3074ec8eefb97
magicdawn <magicdawn@qq.com>
2020-01-05 23:07:37 +0800

    feat: modify web-worker url

--- src/worker/main.js
+++ src/worker/main.js
@@ -1,4 +1,5 @@
-import workUrl from 'file-loader!./worker.js'
+// https://webpack.js.org/loaders/file-loader/#name
+import workUrl from 'file-loader?name=assets/worker/[name].[md5:hash:hex:8].[ext]!./worker.js'
 const worker = new Worker(workUrl)
 
 import * as comlink from 'comlink'
